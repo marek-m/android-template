f93934821589d9be0ee9be506e2030ad72fe8241
Marek <marek>
2015-09-15 13:01:17 +0200

    new templates
    navigation view, recycler view, sub-folders for res menus layouts

---
+++ app/src/main/java/project/template/com/template/template_activity/ExampleActivity.java
@@ -0,0 +1,102 @@
+package project.template.com.template.template_activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import project.template.com.template.R;
+
+/**
+ * Created by Marek on 2015-09-15.
+ */
+public class ExampleActivity extends AppCompatActivity {
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, ExampleActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return intent;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+        setupActionBar();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // action bar menu
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_template_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // action bar menu behaviour
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // TODO
+                return true;
+
+            case R.id.menu_refresh:
+                // TODO
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void setupActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayUseLogoEnabled(false);
+        bar.setDisplayShowTitleEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(false);
+        bar.setHomeButtonEnabled(true);
+    }
+}
